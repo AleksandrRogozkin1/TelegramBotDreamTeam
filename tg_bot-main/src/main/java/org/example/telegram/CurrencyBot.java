0322401ccc68a7163be26deec64ad9d8f6e94a5d
Дмитро Маркевич <119111318+DmitroMarkevich@users.noreply.github.com>
2023-04-14 17:20:41 +0300

    Added a menu with a selection of the number of decimal places

--- tg_bot-main/src/main/java/org/example/telegram/CurrencyBot.java
+++ tg_bot-main/src/main/java/org/example/telegram/CurrencyBot.java
@@ -109,6 +109,9 @@ public class CurrencyBot extends TelegramLongPollingBot {
                         notificationMenuMassage = new MenuCreationService().setNotificationTimeMenu(userId, messageId);
                         execute(notificationMenuMassage);
                         break;
+                    case "GET_DECIMAL_SETTINGS":
+                        EditMessageText precisionMenuMassage = new MenuCreationService().getDecimalPlacesMenu(userId, messageId);
+                        execute(precisionMenuMassage);
                 }
             }
         } catch (TelegramApiException e) {
--- tg_bot-main/src/main/java/org/example/telegram/menu/KeyboardCreationService.java
+++ tg_bot-main/src/main/java/org/example/telegram/menu/KeyboardCreationService.java
@@ -25,7 +25,7 @@ public class KeyboardCreationService {
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         rowsInline.add(createButton("Currency", "GET_CURRENCY_SETTINGS"));
         rowsInline.add(createButton("Bank", "GET_BANK_SETTINGS"));
-        rowsInline.add(createButton("Precision", "GET_PRECISION_SETTINGS"));
+        rowsInline.add(createButton("Precision", "GET_DECIMAL_SETTINGS"));
         rowsInline.add(createButton("Notification time", "GET_NOTIFICATION_SETTINGS"));
         rowsInline.add(createButton("◀️Back", "GET_SETTINGS_BACK"));
         markupInline.setKeyboard(rowsInline);
@@ -35,13 +35,11 @@ public class KeyboardCreationService {
     public InlineKeyboardMarkup getBankKeyboard(long userId) {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-
         rowsInline.add(createButton(checkMarkForBank(userId, Bank.MONOBANK), "SET_MONOBANK"));
         rowsInline.add(createButton(checkMarkForBank(userId, Bank.NBU), "SET_NBU"));
         rowsInline.add(createButton(checkMarkForBank(userId, Bank.PRIVATBANK), "SET_PRIVATBANK"));
         rowsInline.add(createButton("◀️Back", "GET_BANK_BACK"));
         rowsInline.add(createButton("Home", "GET_HOME"));
-
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
@@ -87,6 +85,18 @@ public class KeyboardCreationService {
         return markupInline;
     }
 
+    public InlineKeyboardMarkup getDecimalPlacesKeyboard(long userId) {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        rowsInline.add(createButton("2", "SET_2_DECIMAL"));
+        rowsInline.add(createButton("3", "SET_3_DECIMAL"));
+        rowsInline.add(createButton("4", "SET_4_DECIMAL"));
+        rowsInline.add(createButton("◀️Back", "GET_CURRENCY_BACK"));
+        rowsInline.add(createButton("Home", "GET_HOME"));
+        markupInline.setKeyboard(rowsInline);
+        return markupInline;
+    }
+
     private List<InlineKeyboardButton> createButton(String command, String callBack) {
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
         InlineKeyboardButton button = InlineKeyboardButton.builder()
@@ -102,12 +112,9 @@ public class KeyboardCreationService {
         return userBankSetting == bankName ? bankName + "✅" : bankName.name();
     }
 
-    private Bank getUserBankSetting(long userId) {
-        return FileUtils.getUserSettingsDtoList().stream()
-                .filter(user -> user.getUserId() == userId)
-                .map(User::getCurrentBank)
-                .findFirst()
-                .orElse(Bank.PRIVATBANK);
+    private String checkMarkForCurrency(long userId, Currency currencyName) {
+        Currency userCurrencySetting = getUserCurrencySetting(userId);
+        return userCurrencySetting == currencyName ? currencyName + "✅" : currencyName.name();
     }
 
     /*
@@ -125,9 +132,12 @@ public class KeyboardCreationService {
     }
     */
 
-    private String checkMarkForCurrency(long userId, Currency currencyName) {
-        Currency userCurrencySetting = getUserCurrencySetting(userId);
-        return userCurrencySetting == currencyName ? currencyName + "✅" : currencyName.name();
+    private Bank getUserBankSetting(long userId) {
+        return FileUtils.getUserSettingsDtoList().stream()
+                .filter(user -> user.getUserId() == userId)
+                .map(User::getCurrentBank)
+                .findFirst()
+                .orElse(Bank.PRIVATBANK);
     }
 
     private Currency getUserCurrencySetting(long userId) {
